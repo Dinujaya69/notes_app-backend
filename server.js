ce63dbf4364e954b2cd2a064cec6ccd4b9518e23
Dinujaya69 <kavinduweerasinha4@gmail.com>
2025-05-02 14:08:35 +0530

    create mongo connection

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/dbConfig.js";
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
+connectDB();
 
 app.use(cors());
 app.use(express.json());
